f1e70cec6daa3b8738af173656fd1b5aa0bba18e
Vera Rojman <vrojman@protonmail.com>
2021-02-18 17:55:09 +0100

    Add login icon

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -19,7 +19,7 @@ checksums = [
       "/app/views/layouts/decidim/_mini_footer.html.erb" => "55a9ca723b65b8d9eadb714818a89bb3",
       "/app/views/layouts/decidim/_organization_colors.html.erb" => "34f0d188a62108e7a57a1c270daed8bb",
       "/app/views/decidim/account/show.html.erb" => "2e3c895104e03d7d092467a96f64703d",
-      "/app/views/decidim/devise/sessions/new.html.erb" => "todo"
+      "/app/views/decidim/devise/sessions/new.html.erb" => "1da8569a34bcd014ffb5323c96391837"
     }
   }
 ]
