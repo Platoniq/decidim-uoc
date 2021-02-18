0768d8296dfa532d1c92b9e6095900102178a8cf
Vera Rojman <vrojman@protonmail.com>
2021-02-18 17:43:04 +0100

    Add specs and preserve default login when uoc disabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def uoc?(user)
     user.identities.find_by(provider: "uoc").present?
   end
+
+  def uoc_sign_in_enabled?
+    Rails.application.secrets.omniauth.dig(:uoc, :enabled)
+  end
 end
--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -8,26 +8,53 @@ describe "Visit the login page", type: :system, perform_enqueued: true do
 
   before do
     switch_to_host(organization.host)
-    visit decidim.new_user_session_path
   end
 
-  it "renders the login page" do
-    expect(page).to have_content("Log in")
-  end
-
-  it "doesn't show email and password login by default" do
-    expect(page).not_to have_selector("#session_user_email")
-    expect(page).not_to have_selector("#session_user_password")
-  end
-
-  context "when clicking on 'Enter with another account'" do
+  context "when uoc sign_in is enabled" do
     before do
-      page.find(".alternative-login a").click
+      allow(Rails.application.secrets).to receive(:omniauth).and_return(uoc: { enabled: true })
+      visit decidim.new_user_session_path
+    end
+
+    it "renders the login page" do
+      expect(page).to have_content("Log in")
+    end
+
+    it "doesn't show email and password login by default" do
+      expect(page).not_to have_selector("#session_user_email")
+      expect(page).not_to have_selector("#session_user_password")
+    end
+
+    context "when clicking on 'Enter with another account'" do
+      before do
+        page.find(".alternative-login a").click
+      end
+
+      it "shows email and password login" do
+        expect(page).to have_selector("#session_user_email")
+        expect(page).to have_selector("#session_user_password")
+      end
+    end
+  end
+
+  context "when uoc sign_in is disabled" do
+    before do
+      allow(Rails.application.secrets).to receive(:omniauth).and_return(uoc: { enabled: false })
+      visit decidim.new_user_session_path
+    end
+
+    it "renders the login page" do
+      expect(page).to have_content("Log in")
     end
 
     it "shows email and password login" do
       expect(page).to have_selector("#session_user_email")
       expect(page).to have_selector("#session_user_password")
+    end
+
+    it "has no alternative login link" do
+      expect(page).not_to have_selector(".alternative-login")
+      expect(page).not_to have_content("Enter with another account")
     end
   end
 end
