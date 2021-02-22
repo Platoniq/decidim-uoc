13ef82d9a437f0e8f7fe87620072462b54369702
Ivan Vergés <ivan@platoniq.net>
2021-02-22 17:59:43 +0100

    Duplicate assemblies with the keyword :work_groups

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -23,6 +23,17 @@ checksums = [
       "/app/views/decidim/account/show.html.erb" => "2e3c895104e03d7d092467a96f64703d",
       "/app/views/decidim/devise/sessions/new.html.erb" => "1da8569a34bcd014ffb5323c96391837"
     }
+  },
+  {
+    package: "decidim-assemblies",
+    files: {
+      # just to take into the account if some routes change
+      "/lib/decidim/assemblies/engine.rb" => "99a665d60c949c30a7f127c322dc3de5",
+      "/lib/decidim/assemblies/admin_engine.rb" => "15bfba31a46f70870da42a07b588e59d",
+      "/app/models/decidim/assembly.rb" => "b99256774db6151d5b0e47cacdbea550",
+      "/app/views/decidim/assemblies/_filter_by_type.html.erb" => "76988d76b84d96079e6d9e1b252a3fda",
+      "/app/views/decidim/assemblies/assemblies/_parent_assemblies.html.erb" => "fd026d4ee40dd1d5ebf8ad9ec5d0dbb4"
+    }
   }
 ]
 
--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -5,6 +5,7 @@ require "byebug"
 
 describe "Visit the home page", type: :system, perform_enqueued: true do
   let(:organization) { create :organization }
+  let!(:work_groups) { create :assemblies_type, id: 2 }
 
   before do
     switch_to_host(organization.host)
@@ -12,7 +13,40 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
   end
 
   it "renders the home page" do
-    expect(page).to have_content("Home")
+    within ".main-nav" do
+      expect(page).to have_content("Home")
+      expect(page).not_to have_content("Management")
+      expect(page).not_to have_content("Work groups")
+    end
+  end
+
+  context "when there is normal assemblies" do
+    let!(:assembly) { create(:assembly, :published, organization: organization) }
+
+    it "renders the expected menu" do
+      visit decidim.root_path
+
+      within ".main-nav" do
+        expect(page).to have_content("Home")
+        expect(page).to have_content("Management")
+        expect(page).not_to have_content("Work groups")
+      end
+    end
+  end
+
+  context "when there is alternative assemblies" do
+    let!(:assembly) { create(:assembly, :published, organization: organization) }
+    let!(:assembly2) { create(:assembly, :published, assembly_type: work_groups, organization: organization) }
+
+    it "renders the expected menu" do
+      visit decidim.root_path
+
+      within ".main-nav" do
+        expect(page).to have_content("Home")
+        expect(page).to have_content("Management")
+        expect(page).to have_content("Work groups")
+      end
+    end
   end
 
   describe "logo" do
