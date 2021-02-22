b1c6931974709e76cf6ce994b925682ac26ccff2
Vera Rojman <vrojman@protonmail.com>
2021-02-22 13:13:00 +0100

    Use translated logo file

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -15,6 +15,35 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
     expect(page).to have_content("Home")
   end
 
+  describe "logo" do
+    let(:src) { page.find(".logo-wrapper img")[:src] }
+
+    shared_examples_for "translated logo" do
+      it "loads correct src" do
+        visit decidim.root_path(locale: locale)
+        expect(src).to match "logo_#{locale}"
+      end
+    end
+
+    context "when locale is 'ca'" do
+      let(:locale) { "ca" }
+
+      it_behaves_like "translated logo"
+    end
+
+    context "when locale is 'en'" do
+      let(:locale) { "en" }
+
+      it_behaves_like "translated logo"
+    end
+
+    context "when locale is 'es'" do
+      let(:locale) { "es" }
+
+      it_behaves_like "translated logo"
+    end
+  end
+
   # it "has matomo tracker" do
   #   expect(page.execute_script("return typeof window._paq")).not_to eq("undefined")
   #   expect(page.execute_script("return typeof window._paq")).to eq("object")
