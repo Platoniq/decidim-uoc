f4455742f0228141884c1e6d11a537a8288200ff
Ivan Vergés <ivan@platoniq.net>
2021-02-23 12:24:22 +0100

    Duplicate assemblies with the keyword :work_groups (#21)
    * Duplicate assemblies with the keyword :work_groups
    
    * add translations
    
    * remove override for colors as they cannot be edited anymore
    
    * Split locale files by scope (decidim / uoc)
    
    Co-authored-by: Vera Rojman <vrojman@protonmail.com>

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -17,11 +17,21 @@ checksums = [
     files: {
       # layouts
       "/app/views/layouts/decidim/_mini_footer.html.erb" => "55a9ca723b65b8d9eadb714818a89bb3",
-      "/app/views/layouts/decidim/_organization_colors.html.erb" => "34f0d188a62108e7a57a1c270daed8bb",
       "/app/views/layouts/decidim/_logo.html.erb" => "2713715db652c8107f1fe5f2c4d618b6",
       # views
       "/app/views/decidim/account/show.html.erb" => "2e3c895104e03d7d092467a96f64703d",
       "/app/views/decidim/devise/sessions/new.html.erb" => "1da8569a34bcd014ffb5323c96391837"
+    }
+  },
+  {
+    package: "decidim-assemblies",
+    files: {
+      # just to take into the account if some routes change
+      "/lib/decidim/assemblies/engine.rb" => "99a665d60c949c30a7f127c322dc3de5",
+      "/lib/decidim/assemblies/admin_engine.rb" => "15bfba31a46f70870da42a07b588e59d",
+      "/app/models/decidim/assembly.rb" => "b99256774db6151d5b0e47cacdbea550",
+      "/app/views/decidim/assemblies/_filter_by_type.html.erb" => "76988d76b84d96079e6d9e1b252a3fda",
+      "/app/views/decidim/assemblies/assemblies/_parent_assemblies.html.erb" => "fd026d4ee40dd1d5ebf8ad9ec5d0dbb4"
     }
   }
 ]
