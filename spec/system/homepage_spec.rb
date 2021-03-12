7f0bb82d40b6ba2f514369746a0041156aa01a76
Ivan Vergés <ivan@platoniq.net>
2021-03-12 17:25:15 +0100

    add matomo && etherpad configuration (#36)
    * add matomo && etherpad configuration
    
    * fix spec

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -79,8 +79,10 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
     end
   end
 
-  # it "has matomo tracker" do
-  #   expect(page.execute_script("return typeof window._paq")).not_to eq("undefined")
-  #   expect(page.execute_script("return typeof window._paq")).to eq("object")
-  # end
+  it "has matomo tracker" do
+    visit decidim.root_path
+
+    expect(page.execute_script("return typeof window._paq")).not_to eq("undefined")
+    expect(page.execute_script("return typeof window._paq")).to eq("object")
+  end
 end
